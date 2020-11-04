0e0ac9cbe62c373ef1e57d3951a2f612765e257c
glauberbrack <glauber@brack.com.br>
2020-11-04 00:14:51 -0300

    refactor: using var ons terraform.tfvars and removing it from .gitignore

--- 01 - first-steps/instance.tf
+++ 01 - first-steps/instance.tf
@@ -1,11 +1,6 @@
-provider "aws" {
-  access_key = "YOUR_ACCESS_KEY"
-  secret_key = "YOUR_SECRET_KEY"
-  region     = "eu-west-1"
-}
 
 resource "aws_instance" "example" {
-  ami           = "ami-844e0bf7"
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
 
